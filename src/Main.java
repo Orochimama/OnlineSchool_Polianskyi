d36e078725601d1db1f2a8a813300579417a9644
Volodymyr_Polianskyi <vova95vova95vova95@gmail.com>
2022-10-17 01:21:52 +0300

    First variant

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import com.onlineSchool.classes.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Lecture lecture1 = new Lecture();
+        Lecture lecture2 = new Lecture();
+        Lecture lecture3 = new Lecture();
+
+        System.out.println(Lecture.lectureId);
+    }
+}
---
+++ src/com/onlineSchool/service/AdditionalMaterialsServise.java
@@ -0,0 +1,9 @@
+package com.onlineSchool.service;
+
+import com.onlineSchool.classes.AdditionelMaterials;
+
+public class AdditionalMaterialsServise {
+    public AdditionelMaterials creatAdditionalMaterials() {
+        return new AdditionelMaterials();
+    }
+}
